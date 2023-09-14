11cf475f30717ddc7324d4a2219d00be8432117b
Kent Jasper Cabunoc Sisi <kntjspr@protonmail.ch>
2023-09-15 02:39:08 +0800

    nah

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -5,12 +5,12 @@ export default function sitemap() {
   const postRoutes = allWritings.map((post) => ({
     lastModified: post.publishedAt,
     // REFACTOR: extract url to a constant or use public env variable
-    url: `https://kntjspr.me/writing/${post.slug}`,
+    url: `https://cretu.dev/writing/${post.slug}`,
   }));
 
   const routes = ['', '/writing'].map((route) => ({
     lastModified: new Date().toISOString().split('T')[0],
-    url: `https://kntjspr.me${route}`,
+    url: `https://cretu.dev${route}`,
   }));
 
   return [...routes, ...postRoutes] as MetadataRoute.Sitemap;
