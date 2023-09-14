5552c1e523ad6380c0f6d039c86a84f416e74513
Kent Jasper Cabunoc Sisi <kntjspr@protonmail.ch>
2023-09-14 20:30:58 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -27,8 +27,8 @@ function Header() {
         </div>
       </div>
       <div className="flex flex-col">
-        <h1>Cristian Crețu</h1>
-        <p className="text-quaternary">Design Engineer in Europe</p>
+        <h1>Kent Jasper Cabunoc Sisi</h1>
+        <p className="text-quaternary">Software Developer in Philippines</p>
       </div>
     </div>
   );
@@ -40,26 +40,24 @@ function AboutMe() {
       <p className="text-tertiary">About me</p>
       <div className="text-secondary flex flex-col gap-4">
         <p>
-          Exploring ways to create unique, fluid experiences. Unlimitedly
-          obsessed with solving problems where design and engineering intersect,
-          by creating pixel-perfect, polished interfaces.
+          I'm Kent Jasper Cabunoc Sisi, and I call the lively city of Cagayan de Oro, Philippines, my home.
+          My enthusiasm for learning knows no bounds, especially when it comes to diving into the exciting world of new technologies. Beyond that, you'll often find me engrossed in the pages of books, mingling with friends, breaking a sweat at the gym, or strategizing over a chessboard.
         </p>
         <p>
-          Currently tinkering with various projects and apps. Streamlining a new
-          platform, for the next era of{' '}
-          <ExternalLink href="https://deta.space/docs/en/learn/what-is-pc#content">
-            personal computing
-          </ExternalLink>{' '}
-          at <ExternalLink href="https://deta.space">Deta</ExternalLink>.
-          Previously worked at{' '}
-          <ExternalLink href="https://landmarks.ro">Landmarks</ExternalLink> -
-          building beautiful web apps.
+          Currently, I proudly serve as the Sponsorship Manager at 
+          
+          {' '} <ExternalLink href="https://www.facebook.com/dscustp">
+            Google Developer Student Clubs USTP
+          </ExternalLink>
+          , Operations Department. In this role, I get to connect with brilliant minds, forging relationships and seizing opportunities that fuel innovation and collaboration.
         </p>
         <p>
-          Studying Computer Science. Ardent in reading, writing, and improving
-          consistently through learning. Currently interested in TypeScript and
-          C. Experimenting native apps with Swift. Generating new experiences
-          with Figma.
+        I'm also a freshman at the University of Science and Technology of Southern Philippines, 
+        where I'm pursuing a Bachelor's degree in Computer Science. 
+        My ultimate dream? To build a tech startup that leaves a mark on the world of technology.
+        </p>
+        <p>
+        Currently interested in Reverse Engineering. Creating innovative experiences with Next.js. Fostering novel endeavors with individuals who share a common vision.
         </p>
       </div>
     </div>
